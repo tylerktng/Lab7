b398250095b2bf7f6e5020b004ac4dd48561bf25
Tyler <tkng@ucsd.edu>
2021-05-16 17:52:57 -0700

    Add service worker

--- sw.js
+++ sw.js
@@ -4,3 +4,46 @@
 //   - One for installation
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
+
+const CACHE_NAME = 'cache-lab7';
+const urls = [
+    './',
+    './scripts/',
+    './components/'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache open');
+                cache.addAll(urls);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches
+        .match(event.request)
+        .then(response => {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            return fetch(event.request).then(
+                response => {
+                    if(!response) return response;
+                    let cacheResponse = response.clone();
+                    caches
+                        .open(CACHE_NAME)
+                        .then(cache => cache.put(event.request, cacheResponse));
+                    return response;
+              }
+          );
+        })
+    );
+});
+
+self.addEventListener('activate', event => event.waitUntil(clients.claim()))
